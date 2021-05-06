3c02efc7cb1f58b23c4706b2037784c015217e70
colbytomita <83607990+colbytomita@users.noreply.github.com>
2021-05-06 10:10:18 -0700

    Create Maze.java

---
+++ Maze.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.text.*;
+import java.io.*;
+import java.lang.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.stage.*;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.*;
+import javafx.scene.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.animation.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import java.net.*;
+import javafx.geometry.*;
+
+public class Maze extends Application{
+
+}
